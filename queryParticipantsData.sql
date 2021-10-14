3e6e3dd34b3a42a34bd25f7772a694e375b9207f
dja1716 <hdum@uwaterloo.ca>
2021-10-14 10:59:23 +0900

    Create queryParticipantsData.sql

---
+++ queryParticipantsData.sql
@@ -0,0 +1,6 @@
+select up.username, r.is_vaccinated, up.phone_number from users u, user_profiles up, reservations r, places p
+where u.id = up.fk_user_id
+and r.user_id = u.id
+and p.id = r.place_id
+and r.is_canceled = false
+and p.name = '신촌 독수리다방';
