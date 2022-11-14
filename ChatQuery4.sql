d7ef5769114fe768f810570c295843e3060ab1fa
dja1716 <hdum@uwaterloo.ca>
2022-11-14 16:01:22 +0900

    Create ChatQuery4.sql

---
+++ ChatQuery4.sql
@@ -0,0 +1,9 @@
+select m.created_at, m.content, up2.username, up2.age, up2.university from messages m, user_profiles up1, user_profiles up2
+where m.created_at <= (select m1.created_at from messages m1
+						where m1.room_id = m.room_id
+						order by m1.created_at
+						limit 1)
+and up1.gender = 'Female'
+and up2.gender = 'Male'
+and m.sender_id = up1.fk_user_id
+and m.receiver_id = up2.fk_user_id;
